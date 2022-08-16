f159c0e0a5f24420ad2b304846582019e3b675a6
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-16 10:35:38 +0300

    Initial Commit

---
+++ SwiftUIBootCamp/SwiftUIBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootCampApp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Amby on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
