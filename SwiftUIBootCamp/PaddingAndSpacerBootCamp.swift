629c6f4d95539cd868d614848728b4c9c70d1051
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-16 16:13:06 +0300

    padding with real example

--- SwiftUIBootCamp/PaddingAndSpacerBootCamp.swift
+++ SwiftUIBootCamp/PaddingAndSpacerBootCamp.swift
@@ -9,12 +9,22 @@ import SwiftUI
 
 struct PaddingAndSpacerBootCamp: View {
     var body: some View {
-        Text("Hello, World! This is fun")
-            .background(Color.yellow)
-            //.padding()
-            .padding(.all, 20)
-            .padding(.bottom, 30)
-            .background(Color.blue)
+        VStack(alignment: .leading) {
+            Text("Hello, World!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom, 20)
+
+            Text("This is the description of what we will do on this screen, it is multiple lines and we will alighn the txt to the leading edge")
+        }
+        .padding()
+        .padding(.vertical, 10)
+        .background(
+            Color.white
+                .cornerRadius(10)
+                .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0.0, y: 10)
+        )
+
 
     }
 }
