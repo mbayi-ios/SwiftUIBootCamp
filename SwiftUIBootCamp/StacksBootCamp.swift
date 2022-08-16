ff9fbaf11416a24a7cbefc36dc1336663a190387
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-16 14:15:32 +0300

    vstack, hstack, zstack combined

--- SwiftUIBootCamp/StacksBootCamp.swift
+++ SwiftUIBootCamp/StacksBootCamp.swift
@@ -12,18 +12,36 @@ struct StacksBootCamp: View {
     // HStack -> Horizontal
     // ZStack -> ZInded(back to front)
     var body: some View {
-        ZStack (alignment: .trailing){
+        ZStack(alignment: .top) {
             Rectangle()
-                .fill(Color.red)
-                .frame(width: 200, height: 200)
+                .fill(Color.yellow)
+                .frame(width: 350, height: 500)
 
-            Rectangle()
-                .fill(Color.green)
-                .frame(width: 150, height: 100)
+            VStack {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 150, height: 150)
 
-            Rectangle()
-                .fill(Color.orange)
-                .frame(width: 100, height: 100)
+                Rectangle()
+                    .fill(Color.green)
+                    .frame(width: 100, height: 100)
+
+                HStack {
+                    Rectangle()
+                        .fill(Color.purple)
+                    .frame(width: 50, height: 50)
+
+                    Rectangle()
+                        .fill(Color.pink)
+                        .frame(width: 58, height: 78)
+
+                    Rectangle()
+                        .fill(Color.black)
+                    .frame(width: 50, height: 50)
+                }
+            }
+            .background(Color.black)
+
         }
     }
 }
