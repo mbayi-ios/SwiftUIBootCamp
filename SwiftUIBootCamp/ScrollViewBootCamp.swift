b57d15eb868e7864d551e532e1d06a697881ed0b
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-17 13:57:52 +0300

    scrollView embedded in another scrollView

--- SwiftUIBootCamp/ScrollViewBootCamp.swift
+++ SwiftUIBootCamp/ScrollViewBootCamp.swift
@@ -9,18 +9,23 @@ import SwiftUI
 
 struct ScrollViewBootCamp: View {
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false, content: {
-              VStack {
-                ForEach(0..<50) { index in
-                    Rectangle()
-                        .fill(Color.red)
-                        .frame( height: 300)
+        ScrollView(showsIndicators: false) {
+            ForEach(0..<10) { index in
+                ScrollView(.horizontal, showsIndicators: false, content: {
+                    HStack {
+                        ForEach(0..<30 ) { index in  
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color.white)
+                                .frame(width: 200, height: 200)
+                                .shadow(color: Color.black.opacity(0.1), radius: 10)
+                                .padding()
+                        }
 
-                }
-
+                    }
+                })
 
             }
-        })
+        }
     }
 }
 
