fd170f7025086fe8b7b5385b6c4e332b38bbac74
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-17 22:53:39 +0300

    using @State property wrapper

--- SwiftUIBootCamp/StateBootCamp.swift
+++ SwiftUIBootCamp/StateBootCamp.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct StateBootCamp: View {
 
    @State var backgroundColor: Color = Color.green
+    @State var title: String = "My title"
+    @State var count: Int = 0
 
     var body: some View {
         ZStack {
@@ -19,19 +21,23 @@ struct StateBootCamp: View {
 
             //content
             VStack(spacing: 20) {
-                Text("title")
+                Text(title)
                     .font(.title)
 
-                Text("Count: 1")
+                Text("Count: \(count)")
                     .font(.headline)
                     .underline()
 
                 HStack(spacing: 20) {
                     Button("Button 1") {
                         backgroundColor = .red
+                        title = "button one was pressed"
+                        count += 1
                     }
                     Button("Button 2") {
                         backgroundColor = .purple
+                        title = "button two was pressed"
+                        count -= 1
                     }
                 }
             }
