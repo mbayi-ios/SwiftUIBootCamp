b3600ee1cdde40d9421e322a9338d688264865ec
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-17 15:50:30 +0300

    working with buttons

--- SwiftUIBootCamp/ButtonsBootCamp.swift
+++ SwiftUIBootCamp/ButtonsBootCamp.swift
@@ -17,11 +17,51 @@ struct ButtonsBootCamp: View {
             Button("Click me!") {
                 self.title = "button was pressed"
             }
+            .accentColor(.red)
 
             Button(action: {
                 self.title = "button Two was pressed"
             }, label: {
-                Text("Button 2")
+                Text("Button 2".uppercased())
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(
+                        Color.blue
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                    )
+            })
+
+            Button(action: {
+                self.title = "Button 3"
+            }, label: {
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 100, height: 100)
+                    .shadow(radius: 10)
+                    .overlay(
+                        Image(systemName: "heart.fill")
+                            .font(.largeTitle)
+
+                    )
+            })
+
+
+            Button(action: {
+                self.title = "finished"
+            }, label: {
+                Text("Finished".uppercased())
+                    .font(.caption)
+                    .bold()
+                    .foregroundColor(.gray)
+                    .padding()
+                    .padding(.horizontal, 10)
+                    .background(
+                        Capsule()
+                            .stroke(Color.gray, lineWidth: 2.0)
+                    )
             })
 
         }
