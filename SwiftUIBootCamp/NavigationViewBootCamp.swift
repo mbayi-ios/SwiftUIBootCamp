e78886885feaffbb3ce48c15f652333af8c79d9f
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-19 00:10:00 +0300

    working with navigation view

---
+++ SwiftUIBootCamp/NavigationViewBootCamp.swift
@@ -0,0 +1,31 @@
+//
+//  NavigationViewBootCamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Amby on 19/08/2022.
+//
+
+import SwiftUI
+
+struct NavigationViewBootCamp: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                Text("Hello world")
+                Text("Hello world")
+                Text("Hello world")
+            }
+            .navigationTitle("All Inboxes")
+            //.navigationBarTitleDisplayMode(.inline)
+            //.navigationBarTitleDisplayMode(.large)
+            .navigationBarTitleDisplayMode(.automatic)
+
+        }
+    }
+}
+
+struct NavigationViewBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationViewBootCamp()
+    }
+}
