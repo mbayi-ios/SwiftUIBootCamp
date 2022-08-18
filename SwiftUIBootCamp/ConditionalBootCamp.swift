e202131c9c6663118369b5c3e5bce84b99d166ab
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-18 12:58:47 +0300

    loading progress view

--- SwiftUIBootCamp/ConditionalBootCamp.swift
+++ SwiftUIBootCamp/ConditionalBootCamp.swift
@@ -10,20 +10,34 @@ import SwiftUI
 struct ConditionalBootCamp: View {
 
     @State var showCircle: Bool = false
+    @State var showRectangle: Bool = false
+    @State var isLoading: Bool = false
 
     var body: some View {
         VStack (spacing: 20){
-            Button("Circle Button: \(showCircle.description)") {
-                showCircle.toggle()
-            }
 
-            if showCircle {
-                Circle()
-                    .frame(width: 100, height: 100)
-            } else {
-                Rectangle()
-                    .frame(width: 100, height: 100)
+            Button("IS LOADING: \(isLoading.description)") {
+                isLoading.toggle()
             }
+            if isLoading {
+                ProgressView()
+            }
+//            Button("Circle Button: \(showCircle.description)") {
+//                showCircle.toggle()
+//            }
+//            Button("Rectangle Button: \(showRectangle.description)") {
+//                showRectangle.toggle()
+//            }
+//            if showCircle {
+//                Circle()
+//                    .frame(width: 100, height: 100)
+//            } else if showRectangle {
+//                Rectangle()
+//                    .frame(width: 100, height: 100)
+//            } else {
+//                RoundedRectangle(cornerRadius: 25)
+//                    .frame(width: 200, height: 100)
+//            }
 
 
         }
