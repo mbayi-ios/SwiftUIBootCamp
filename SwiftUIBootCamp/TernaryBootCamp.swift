758a538525a3228149c40581328aa51e83add956
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-18 13:15:03 +0300

    working with ternary operators

---
+++ SwiftUIBootCamp/TernaryBootCamp.swift
@@ -0,0 +1,29 @@
+//
+//  TernaryBootCamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Amby on 18/08/2022.
+//
+
+import SwiftUI
+
+struct TernaryBootCamp: View {
+    @State var isStartingState: Bool = false
+    var body: some View {
+
+        VStack {
+            Button("Button") {
+                isStartingState.toggle()
+            }
+            RoundedRectangle(cornerRadius: 20)
+                .fill(isStartingState ? Color.red : Color.blue)
+                .frame(width: 200, height: 100)
+        }
+    }
+}
+
+struct TernaryBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TernaryBootCamp()
+    }
+}
