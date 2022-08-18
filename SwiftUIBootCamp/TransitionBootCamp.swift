0ee9e61c2ef6b11a0c8a5701bb9eeb2fa58beeb6
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-18 23:09:13 +0300

    using transitions

--- SwiftUIBootCamp/TransitionBootCamp.swift
+++ SwiftUIBootCamp/TransitionBootCamp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct TransitionBootCamp: View {
     @State var showView: Bool = false
-    
+
     var body: some View {
         ZStack(alignment: .bottom) {
             VStack {
@@ -18,10 +18,20 @@ struct TransitionBootCamp: View {
                 }
                 Spacer()
             }
-            RoundedRectangle(cornerRadius: 30)
-                .frame(height: UIScreen.main.bounds.height * 0.5)
-                .opacity(showView ? 1.0 : 0.0)
-                .animation(.easeInOut)
+
+            if showView {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(height: UIScreen.main.bounds.height * 0.5)
+                    //.transition(.slide)
+                    //.transition(.move(edge: .bottom))
+                    //.transition(.move(edge: .top))
+                    //.animation(.easeInOut)
+                    //.animation(.spring())
+                    //.transition(AnyTransition.opacity.animation(.easeInOut))
+                    //.transition(AnyTransition.scale.animation(.easeInOut))
+                    .transition(.asymmetric(insertion: .move(edge: .leading), removal: .move(edge: .bottom)))
+
+            }
         }
         .edgesIgnoringSafeArea(.bottom)
     }
