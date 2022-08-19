dd011d0f221d212ea4d1a5f6bd7a72da25bea14e
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-19 14:45:54 +0300

    display default actionSheet

---
+++ SwiftUIBootCamp/ActionSheetBootCamp.swift
@@ -0,0 +1,26 @@
+//
+//  ActionSheetBootCamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Amby on 19/08/2022.
+//
+
+import SwiftUI
+
+struct ActionSheetBootCamp: View {
+    @State var showActionSheet: Bool = false
+    var body: some View {
+        Button("click me") {
+            showActionSheet.toggle()
+        }
+        .actionSheet(isPresented: $showActionSheet, content: {
+            ActionSheet(title: Text("this is the title"))
+        })
+    }
+}
+
+struct ActionSheetBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ActionSheetBootCamp()
+    }
+}
