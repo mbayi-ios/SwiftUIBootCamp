cbf520d292b74f16d586fb1e4c4b3b0b648da4e0
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-19 13:36:49 +0300

    adding alert with dynamic messages

--- SwiftUIBootCamp/AlertBootCam.swift
+++ SwiftUIBootCamp/AlertBootCam.swift
@@ -11,28 +11,46 @@ struct AlertBootCam: View {
     @State var showAlert: Bool = false
     @State var backgroundColor: Color = Color.yellow
 
+    @State var alertTitle: String = ""
+    @State var alertMessage: String = ""
+
     var body: some View {
         ZStack {
             backgroundColor.edgesIgnoringSafeArea(.all)
-            Button("Click Here") {
-                showAlert.toggle()
+            VStack(spacing: 50) {
+                Button("Button 1") {
+                    alertTitle = "error uploading video"
+                    alertMessage = "the vidoe could not be uploadded"
+                    showAlert.toggle()
+                }
+
+                Button("Button 2") {
+                    alertTitle = "successfully uploaded video!😄"
+                    alertMessage = "Your video is now public"
+                    showAlert.toggle()
+                }
+
+
+
             }
             .alert(isPresented: $showAlert) {
                 //Alert(title: Text("there was an error!"))
                 getAlert()
-
-        }
+            }
         }
     }
 
     func getAlert() -> Alert {
-        return  Alert(
-            title: Text("This is the title"),
-            message: Text("here we will describe the error"),
-            primaryButton: .cancel(),
-            secondaryButton: .destructive(Text("delete"), action: {
-                backgroundColor = .red
-            }))
+        return Alert(title: Text(alertTitle),
+        message: Text(alertMessage),
+                     dismissButton: .default(Text("ok")))
+//        return  Alert(
+//            title: Text("This is the title"),
+//            message: Text("here we will describe the error"),
+//            primaryButton: .cancel(),
+//            secondaryButton: .destructive(Text("delete"), action: {
+//                backgroundColor = .red
+//            }))
     }
 }
 
