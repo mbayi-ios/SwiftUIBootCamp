b9916c373076129d9963a71db280ec8151d679cb
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-19 20:49:04 +0300

    working with date formatter

--- SwiftUIBootCamp/DatePickerBootCamp.swift
+++ SwiftUIBootCamp/DatePickerBootCamp.swift
@@ -9,14 +9,30 @@ import SwiftUI
 
 struct DatePickerBootCamp: View {
     @State var selectedDate: Date = Date()
+    let startingDate: Date = Calendar.current.date(from: DateComponents(year: 2018)) ?? Date()
+    let endingDate: Date = Date()
+
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        return formatter
+    }
+
     var body: some View {
-        DatePicker("select a Date", selection: $selectedDate)
-            .accentColor(Color.red)
-            .datePickerStyle(
-                //CompactDatePickerStyle()
-                //GraphicalDatePickerStyle()
-                WheelDatePickerStyle()
-            )
+        VStack {
+            Text("SELECTED DATE IS: ")
+            Text(dateFormatter.string(from: selectedDate))
+                .font(.title)
+            //DatePicker("select a Date", selection: $selectedDate)
+            DatePicker("select a date", selection: $selectedDate, in: startingDate...endingDate, displayedComponents: [.date])
+                .accentColor(Color.red)
+                .datePickerStyle(
+                    CompactDatePickerStyle()
+                    //GraphicalDatePickerStyle()
+                    //WheelDatePickerStyle()
+                )
+        }
+
     }
 }
 
