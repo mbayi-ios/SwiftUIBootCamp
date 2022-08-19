d6e8d734bc3527ab5c73dad046f8be162feae69d
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-19 19:14:27 +0300

    working with menupicker

--- SwiftUIBootCamp/PickerBootCamp.swift
+++ SwiftUIBootCamp/PickerBootCamp.swift
@@ -8,27 +8,56 @@
 import SwiftUI
 
 struct PickerBootCamp: View {
-    @State var selection: String = "1"
+    @State var selection: String = "Most Recent"
+    let filterOptions: [String] = [
+    "Most Recent", "Most Popular", "Most Liked"
+    ]
     var body: some View {
-        VStack {
-            HStack {
-                Text("Age: " )
-                Text(selection)
+        Picker(selection: $selection,
+               label:
+                HStack {
+            Text("Filter")
+            Text(selection)
             }
-            Picker(
-                selection: $selection,
-                label: Text("Picker"),
-                content: {
-                    //Text("1").tag("1")
-                    //Text("2").tag("12")
-                    ForEach(18..<100) { number in
-                        Text("\(number)")
-                            .tag("\(number)")
-                    }
-            })
-                .pickerStyle(WheelPickerStyle())
-                .background(Color.gray.opacity(0.3))
-        }
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .padding(.horizontal)
+                .background(Color.blue)
+                .cornerRadius(10)
+                .shadow(color: Color.blue, radius: 10, x: 0, y: 10)
+               ,
+               content: {
+            ForEach(filterOptions, id: \.self) {option in
+                HStack {
+                    Text(option)
+                    Image(systemName: "heart.fill")
+
+                }
+                .tag(option)
+            }
+        })
+            .pickerStyle(MenuPickerStyle())
+
+//        VStack {
+//            HStack {
+//                Text("Age: " )
+//                Text(selection)
+//            }
+//            Picker(
+//                selection: $selection,
+//                label: Text("Picker"),
+//                content: {
+//                    //Text("1").tag("1")
+//                    //Text("2").tag("12")
+//                    ForEach(18..<100) { number in
+//                        Text("\(number)")
+//                            .tag("\(number)")
+//                    }
+//            })
+//                .pickerStyle(WheelPickerStyle())
+//                .background(Color.gray.opacity(0.3))
+//        }
     }
 }
 
