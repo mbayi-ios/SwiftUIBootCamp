7ae0c071cb643482d33fb26d8c00846a6346f3b6
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-19 23:17:16 +0300

    setting up tab with clickable button

--- SwiftUIBootCamp/TabViewBootCamp.swift
+++ SwiftUIBootCamp/TabViewBootCamp.swift
@@ -8,37 +8,57 @@
 import SwiftUI
 
 struct TabViewBootCamp: View {
+
+    @State var selectedTab: Int = 2
+
     var body: some View {
-        TabView {
-            HomeView()
+        TabView(selection: $selectedTab) {
+            HomeView(selectedTab: $selectedTab)
                 .tabItem {
                     Image(systemName: "house.fill")
                     Text("Home")
                 }
+                .tag(0)
 
             Text("BROWSE TAB")
                 .tabItem {
                     Image(systemName: "globe")
                     Text("Browse")
                 }
+                .tag(1)
 
             Text("PROFILE TAB")
                 .tabItem {
-                    Image(systemName: "house.fill")
+                    Image(systemName: "person.fill")
                     Text("Profile")
                 }
+                .tag(2)
         }
         .accentColor(.black)
     }
 }
 
 struct HomeView: View {
+    @Binding var selectedTab: Int
     var body: some View {
         ZStack {
             Color.purple.ignoresSafeArea()
-            Text("HOME TAB")
-                .font(.largeTitle)
-                .foregroundColor(.white)
+            VStack {
+                Text("HOME TAB")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+
+                Button(action: {
+                    selectedTab = 2
+                }, label: {
+                    Text("Go to profile")
+                        .font(.headline)
+                        .padding()
+                        .padding(.horizontal)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                })
+            }
         }
 
     }
