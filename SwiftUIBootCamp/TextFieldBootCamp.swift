ae95997e8ddbdff7365e55b3897679f12a4d9d7d
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-19 17:32:57 +0300

    using text field and disabling button

--- SwiftUIBootCamp/TextFieldBootCamp.swift
+++ SwiftUIBootCamp/TextFieldBootCamp.swift
@@ -22,16 +22,21 @@ struct TextFieldBootCamp: View {
                 .font(.headline)
 
                 Button(action: {
-                    saveText()
+                    if textIsAppropriate() {
+                        saveText()
+                    }
+
                 }, label: {
                     Text("Save".uppercased())
                         .padding()
                         .frame(maxWidth: .infinity)
-                        .background(Color.blue.cornerRadius(10))
+                        .background(textIsAppropriate() ? Color.blue : Color.gray)
+                        .cornerRadius(10)
                         .foregroundColor(.white)
 
                         .font(.headline)
                 })
+                    .disabled(!textIsAppropriate())
 
                 ForEach(dataArray, id: \.self) { data in
                     Text(data)
@@ -41,6 +46,13 @@ struct TextFieldBootCamp: View {
             .padding()
             .navigationTitle("Textfield Bootcamp")
         }
+    }
+
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count >= 3 {
+            return true
+        }
+        return false
     }
 
     func saveText() {
