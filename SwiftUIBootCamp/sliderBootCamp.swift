bd0fe26a79b9404e6870a3e962580cf859b58a2f
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-19 22:56:48 +0300

    working with sliders

--- SwiftUIBootCamp/sliderBootCamp.swift
+++ SwiftUIBootCamp/sliderBootCamp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct sliderBootCamp: View {
     @State var sliderValue: Double = 3
+    @State var color: Color = .red
     var body: some View {
         VStack {
             Text("Rating: ")
@@ -18,9 +19,23 @@ struct sliderBootCamp: View {
             )
             //Slider(value: $sliderValue)
             //Slider(value: $sliderValue, in: 1...5)
-            Slider(value: $sliderValue, in: 1...5, step: 0.5)
+            //Slider(value: $sliderValue, in: 1...5, step: 0.5)
+            Slider(
+                value: $sliderValue,
+                in: 1...5,
+                step: 1.0,
+                onEditingChanged: { (_) in
+                    color = .green
+                },
+            minimumValueLabel: Text("1"),
+                maximumValueLabel: Text("5") ,
+                label: {
+                    Text("Title")
+                })
                 .accentColor(.red)
+
         }
+        .padding(.horizontal, 50)
     }
 }
 
