f17a0d7747268506f463c15c698278f21776f126
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-19 20:27:08 +0300

    working with default date picker

---
+++ SwiftUIBootCamp/DatePickerBootCamp.swift
@@ -0,0 +1,21 @@
+//
+//  DatePickerBootCamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Amby on 19/08/2022.
+//
+
+import SwiftUI
+
+struct DatePickerBootCamp: View {
+    @State var selectedDate: Date = Date()
+    var body: some View {
+        DatePicker("select a Date", selection: $selectedDate)
+    }
+}
+
+struct DatePickerBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        DatePickerBootCamp()
+    }
+}
