fccc93a97703a31be44c02fd31cfb6cc6ea64d04
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-21 08:45:25 +0300

    using custom data model to display users

--- SwiftUIBootCamp/ModelBootCamp.swift
+++ SwiftUIBootCamp/ModelBootCamp.swift
@@ -7,19 +7,49 @@
 
 import SwiftUI
 
+struct UserModel: Identifiable {
+    let id: String = UUID().uuidString
+    let displayName: String
+    let userName: String
+    let followerCount: Int
+    let isVerified: Bool
+}
+
 struct ModelBootCamp: View {
-    @State var users: [String] = [
-        "Nick", "Emily", "Chris"
+    @State var users: [UserModel] = [
+        UserModel(displayName: "Nick", userName: "nick123", followerCount: 4, isVerified: true),
+        UserModel(displayName: "Emily", userName: "Emily123", followerCount: 4, isVerified: false),
+        UserModel(displayName: "Samantha", userName: "Ninja", followerCount: 3, isVerified: true)
     ]
     var body: some View {
         NavigationView {
             List {
-                ForEach(users, id: \.self) { name in
+                ForEach(users) { user in
 
                     HStack(spacing: 15) {
                         Circle()
                             .frame(width:35, height: 35)
-                        Text(name)
+                        VStack(alignment: .leading) {
+                            Text(user.displayName)
+                                .font(.headline)
+                            Text("@\(user.userName)")
+                                .foregroundColor(.gray)
+                                .font(.caption)
+
+                        }
+                        Spacer()
+                        if user.isVerified {
+                            Image(systemName: "checkmark.seal.fill")
+                                .foregroundColor(.blue)
+                        }
+
+                        VStack {
+                            Text("\(user.followerCount)")
+                                .font(.headline)
+                            Text("Followers")
+                                .foregroundColor(.gray)
+                                .font(.caption)
+                        }
                     }
                     .padding(.vertical, 10)
                 }
