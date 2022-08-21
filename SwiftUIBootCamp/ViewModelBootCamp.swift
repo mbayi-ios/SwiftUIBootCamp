0db6911f15ab3ff55dd67e7c3ac831cd7c83160a
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-21 09:50:13 +0300

    using observedObject and StateObject

--- SwiftUIBootCamp/ViewModelBootCamp.swift
+++ SwiftUIBootCamp/ViewModelBootCamp.swift
@@ -16,22 +16,26 @@ struct FruitModel: Identifiable {
 class FruitViewModel: ObservableObject {
     @Published var fruitArray: [FruitModel] = []
     @Published var isLoading: Bool = false
-    
+
+    init(){
+        getFruits()
+    }
+
     func deleteFruit(index: IndexSet) {
         fruitArray.remove(atOffsets: index)
     }
-    
+
     func getFruits(){
         let fruit1 = FruitModel(name: "Orange", count: 1)
         let fruit2 = FruitModel(name: "Banana", count: 4)
         let fruit3 = FruitModel(name: "peach", count: 3)
-        
+
         isLoading = true
         DispatchQueue.main.asyncAfter(deadline: .now() + 3.0){
             self.fruitArray.append(fruit1)
             self.fruitArray.append(fruit2)
             self.fruitArray.append(fruit3)
-            
+
             self.isLoading = false
         }
     }
@@ -41,12 +45,15 @@ struct ViewModelBootCamp: View {
     //    @State var fruitArray: [FruitModel] = [
     //        FruitModel(name: "Apples", count: 5)
     //    ]
-    @ObservedObject var fruitViewModel: FruitViewModel = FruitViewModel()
-    
+    // @StateObject -> USE THIS ON CREATIION / INIT
+    // @ObservedObject -> USE THIS FOR SUBVIEWS
+
+    @StateObject var fruitViewModel: FruitViewModel = FruitViewModel()
+
     var body: some View {
         NavigationView {
             List {
-                
+
                 if fruitViewModel.isLoading {
                     ProgressView()
                 } else  {
@@ -63,17 +70,44 @@ struct ViewModelBootCamp: View {
                 }
             }
             .navigationTitle("Fruit List")
-            .onAppear {
-                fruitViewModel.getFruits()
+            .navigationBarItems( trailing:
+                                    NavigationLink(
+                                        destination: RandomScreen(fruitViewModel: fruitViewModel),
+                                        label: {
+                                            Image(systemName: "arrow.right")
+                                                .font(.title)
+                                        })
+            )
+
+        }
+    }
+
+}
+
+struct RandomScreen: View {
+
+    @Environment(\.presentationMode) var presentationMode
+
+    @ObservedObject var fruitViewModel: FruitViewModel
+
+    var body: some View {
+        ZStack {
+            Color.green.ignoresSafeArea()
+
+            VStack {
+                ForEach(fruitViewModel.fruitArray) { fruit in
+                    Text(fruit.name )
+                        .foregroundColor(.white)
+                        .font(.headline)
+                }
             }
         }
     }
-    
-    
 }
 
 struct ViewModelBootCamp_Previews: PreviewProvider {
     static var previews: some View {
         ViewModelBootCamp()
+
     }
 }
