89ee09b477ac7a9902c422ee06d4df7811871f09
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-21 08:11:44 +0300

    using tap gesture

---
+++ SwiftUIBootCamp/TapGestureBootCamp.swift
@@ -0,0 +1,50 @@
+//
+//  TapGestureBootCamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Amby on 21/08/2022.
+//
+
+import SwiftUI
+
+struct TapGestureBootCamp: View {
+    @State var isSelected: Bool = false
+    var body: some View {
+        VStack(spacing: 40) {
+            RoundedRectangle(cornerRadius: 25)
+                .frame(height: 200)
+                .foregroundColor(isSelected ? Color.green : Color.red)
+            Button(action: {
+                isSelected.toggle()
+            }, label: {
+                Text("Button")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(25)
+            })
+
+            Text("Tap Gesture")
+                .font(.headline)
+                .foregroundColor(.white)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .cornerRadius(25)
+                .onTapGesture {
+                    isSelected.toggle()
+                }
+
+            Spacer()
+        }
+        .padding(40)
+    }
+}
+
+struct TapGestureBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TapGestureBootCamp()
+    }
+}
