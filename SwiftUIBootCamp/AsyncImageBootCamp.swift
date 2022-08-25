02d2dd7ba74b2120830053b6286d6c6d05a0088b
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-25 13:16:58 +0300

    using diffrent asyncImage handler

--- SwiftUIBootCamp/AsyncImageBootCamp.swift
+++ SwiftUIBootCamp/AsyncImageBootCamp.swift
@@ -10,17 +10,38 @@ import SwiftUI
 struct AsyncImageBootCamp: View {
     let url  = URL(string: "https://picsum.photos/400")
     var body: some View {
-        AsyncImage(url: url, content: { returnedImage in
-            returnedImage
-                .resizable()
-                .scaledToFit()
-                .frame(width: 400, height: 400)
-                .cornerRadius(20)
+        AsyncImage(url: url) { phase in
+            switch phase {
+            case .empty:
+                ProgressView()
 
-        }, placeholder: {
-            ProgressView()
-        })
-            .frame(width: 100, height: 100)
+            case .success(let returnedImage):
+                returnedImage
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 400, height: 400)
+                    .cornerRadius(20)
+
+            case .failure:
+                Image(systemName: "questionmark")
+                    .font(.headline)
+
+            default:
+                Image(systemName: "questionmark")
+                    .font(.headline)
+
+            }
+        }
+//        AsyncImage(url: url, content: { returnedImage in
+//            returnedImage
+//                .resizable()
+//                .scaledToFit()
+//                .frame(width: 400, height: 400)
+//                .cornerRadius(20)
+//
+//        }, placeholder: {
+//            ProgressView()
+//        })
     }
 }
 
